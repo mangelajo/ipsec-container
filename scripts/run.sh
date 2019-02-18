0c9d93b8f2ae1cbe583e6a8781f4a4af8a53abe6
Miguel Angel Ajo <majopela@redhat.com>
2019-02-18 14:22:59 +0100

    non-ipsec support

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,70 +3,102 @@ set -x
 
 # Input parameters
 
+IPSEC_ENABLED=${IPSEC_ENABLED:-yes}
 PSK=${PSK:-uazioghXBFsKmOEy9tMKjGE9G3o61iGpEmquNf28xt4inDVIume1fkyEZk2B79rG}
-IPSEC_SIDE=${IPSEC_SIDE:-left}
+SIDE=${SIDE:-$IPSEC_SIDE}
+SIDE=${SIDE:-left}
 VXLAN_PORT=${VXLAN_PORT:-4789}
 REMOTE_IP=${REMOTE_IP:-NOIP}
 
 # Optional parameter tweaks
 
-LEFT_OVERLAY_IP=${LEFT_OVERLAY_IP:-169.254.0.1}
-RIGHT_OVERLAY_IP=${RIGHT_OVERLAY_IP:-169.254.0.2}
+if [[ "$IPSEC_ENABLED" != "yes" ]]; then
+    LEFT_OVERLAY_IP=${LEFT_OVERLAY_IP:-169.254.1.1}
+    RIGHT_OVERLAY_IP=${RIGHT_OVERLAY_IP:-169.254.1.2}
+else
+    LEFT_OVERLAY_IP=${LEFT_OVERLAY_IP:-169.254.0.1}
+    RIGHT_OVERLAY_IP=${RIGHT_OVERLAY_IP:-169.254.0.2}
+fi
 
 
 if [[ "$REMOTE_IP" == "NOIP" ]]; then
 
    echo error: please, specify a REMOTE_IP env variable
-   exit 1 
+   exit 1
 fi
 
+# TODO: pull setup.sh here
 source /setup.sh
 
-if [[ "$IPSEC_SIDE" == "left" ]]; then 
-	ipsec auto --up mytunnel
-	sleep 5
-fi
+iperf -s -p 30024 -D -w 4M
 
-# Wait for the tunnel to go up
+#
+# IPSEC Handling
+#
 
-while true
-do
-    ip tunnel | grep -q vti01 && break
-    sleep 1
-done
+if [[ "$IPSEC_ENABLED" == "yes" ]]; then
 
-
-# Assign an LLE IP over the tunnel connection
-
-if [[ "$IPSEC_SIDE" == "left" ]]; then
-  MY_IP=169.254.1.1/30
-else
-  MY_IP=169.254.1.2/30
-  iperf -s -p 30024 -D -w 4M
-fi
-
-ip l set dev vti01 up
-ip a add dev vti01 $MY_IP
-
-# ensure that TCP connections going through the tunnel get the MSS adjusted accordingly to
-# the tunnel MTU
-
-MSS=$((eth0_mtu - VXLAN_OVERHEAD - IPSEC_OVERHEAD - IP_TCP_OVERHEAD))
-
-iptables -t mangle -A FORWARD -o vti01 -p tcp -m tcp --tcp-flags SYN,RST SYN \
- 	-m tcpmss --mss $MSS:1536 -j TCPMSS --set-mss $MSS
-
-if [ -f /configuration/routes.sh ]
-then
-    /configuration/routes.sh
-fi
-
-# SRC_VXLAN_PORT=$(tcpdump -i eth0 -nn port 4789 -c 1 -Q in 2>/dev/null | head -n 1 | cut -d\  -f 3 | cut -d. -f 5)
-
-while true
-do
-    if [[ "$IPSEC_SIDE" == "left" ]]; then
-       ipsec whack --trafficstatus | grep -q '"mytunnel"' || echo "TUNNEL DISCONNECTED"
+    # bring up the connection from the left side
+    if [[ "$SIDE" == "left" ]]; then
+        ipsec auto --up mytunnel
+        sleep 5
     fi
-    sleep 5
-done
+
+    # Wait for the tunnel to go up
+
+    while true
+    do
+        ip tunnel | grep -q vti01 && break
+        sleep 1
+    done
+
+    # Assign an LLE IP over the tunnel connection
+
+    if [[ "$SIDE" == "left" ]]; then
+      MY_IP=169.254.1.1/30
+    else
+      MY_IP=169.254.1.2/30
+    fi
+
+    ip l set dev vti01 up
+    ip a add dev vti01 $MY_IP
+
+    # ensure that TCP connections going through the tunnel get the MSS adjusted accordingly to
+    # the tunnel MTU
+
+    MSS=$((eth0_mtu - VXLAN_OVERHEAD - IPSEC_OVERHEAD - IP_TCP_OVERHEAD))
+
+    iptables -t mangle -A FORWARD -o vti01 -p tcp -m tcp --tcp-flags SYN,RST SYN \
+        -m tcpmss --mss $MSS:1536 -j TCPMSS --set-mss $MSS
+
+    if [ -f /configuration/routes.sh ]
+    then
+        /configuration/routes.sh
+    fi
+
+    # SRC_VXLAN_PORT=$(tcpdump -i eth0 -nn port 4789 -c 1 -Q in 2>/dev/null | head -n 1 | cut -d\  -f 3 | cut -d. -f 5)
+
+    while true
+    do
+        if [[ "$SIDE" == "left" ]]; then
+           ipsec whack --trafficstatus | grep -q '"mytunnel"' || echo "TUNNEL DISCONNECTED"
+        fi
+        sleep 5
+    done
+else
+    MSS=$((eth0_mtu - VXLAN_OVERHEAD - IP_TCP_OVERHEAD))
+
+    iptables -t mangle -A FORWARD -o vxlan42 -p tcp -m tcp --tcp-flags SYN,RST SYN \
+        -m tcpmss --mss $MSS:1536 -j TCPMSS --set-mss $MSS
+
+    if [ -f /configuration/routes.sh ]
+    then
+        /configuration/routes.sh
+    fi
+
+    while true
+    do
+        sleep 5
+    done
+fi
+
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,18 +17,18 @@ if [[ "x$VXLAN_BASE_PORT" != "x" ]]; then
 fi
 
 
-if [[ "$IPSEC_SIDE" == "left" ]]; then
+if [[ "$SIDE" == "left" ]]; then
    local_overlay_ip=$LEFT_OVERLAY_IP
    echo $LEFT_OVERLAY_IP $RIGHT_OVERLAY_IP : PSK \"$PSK\" > /etc/ipsec.secrets
    srcport1=$((VXLAN_PORT + 30))
    srcport2=$((VXLAN_PORT + 40))
-   dst_ipsec=169.254.1.2
+   dst_ip=169.254.1.2
 else
    local_overlay_ip=$RIGHT_OVERLAY_IP
    echo $RIGHT_OVERLAY_IP $LEFT_OVERLAY_IP : PSK \"$PSK\" > /etc/ipsec.secrets
    srcport1=$((VXLAN_PORT + 10))
    srcport2=$((VXLAN_PORT + 20))
-   dst_ipsec=169.254.1.1
+   dst_ip=169.254.1.1
 fi
 
 # CLEANUP conntrack for our UDP port before starting
@@ -42,11 +42,13 @@ ip link set dev vxlan42 mtu $((eth0_mtu - VXLAN_OVERHEAD))
 ip addr add $local_overlay_ip/24 dev vxlan42
 ip link set up vxlan42
 
-ethtool -K eth0 tx-checksum-ip-generic off
+if [[ "$IPSEC_ENABLED" == "yes" ]]; then
+    # otherwise ipsec on top of VXLAN packets land ethernet with the wrong checksum
+    # kernel bug
+    ethtool -K eth0 tx-checksum-ip-generic off
 
 # WRITE IPSEC CONFIG
-
-cat <<EOF > /etc/ipsec.conf
+    cat <<EOF > /etc/ipsec.conf
 config setup
         virtual_private=%v4:10.0.0.0/8,%v4:192.168.0.0/16,%v4:172.16.0.0/12,%v4:25.0.0.0/8,%v4:100.64.0.0/10,%v6:fd00::/8,%v6:fe80::/10
         protostack=netkey
@@ -70,18 +72,19 @@ conn mytunnel
     #rightupdown=/updown.sh
 EOF
 
-# Check configuration file
-/usr/libexec/ipsec/addconn --config /etc/ipsec.conf --checkconfig
-# Check for kernel modules
-/usr/libexec/ipsec/_stackmanager start
-# Check for nss database status and migration
-/usr/sbin/ipsec --checknss
-# Check for nflog setup
-/usr/sbin/ipsec --checknflog
-# Start the actual IKE daemon
-/usr/libexec/ipsec/pluto --leak-detective --config /etc/ipsec.conf #--nofork
-sleep 5
-ipsec auto --add mytunnel
+    # Check configuration file
+    /usr/libexec/ipsec/addconn --config /etc/ipsec.conf --checkconfig
+    # Check for kernel modules
+    /usr/libexec/ipsec/_stackmanager start
+    # Check for nss database status and migration
+    /usr/sbin/ipsec --checknss
+    # Check for nflog setup
+    /usr/sbin/ipsec --checknflog
+    # Start the actual IKE daemon
+    /usr/libexec/ipsec/pluto --leak-detective --config /etc/ipsec.conf #--nofork
+    sleep 5
+    ipsec auto --add mytunnel
+fi # IPSEC_ENABLED == yes
 
 # redirect local ports 100:200 back and forth
 
@@ -89,8 +92,8 @@ for proto in udp tcp; do
 
    iptables -t nat -A PREROUTING -p $proto -i eth0 -m multiport \
             --dports 100:200 \
-            -j DNAT --to-destination $dst_ipsec
-   iptables -A FORWARD -p $proto -d $dst_ipsec -m multiport \
+            -j DNAT --to-destination $dst_ip
+   iptables -A FORWARD -p $proto -d $dst_ip -m multiport \
             --dports 100:200 \
             -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 done
